3bce468850d36cdc9a82ad4cace37417a8b58bbc
chandrakhumar <civil.chandra26@gmail.com>
2024-05-29 18:09:16 +0530

    update project

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+
+import Navbar from "./Navbar"
+import Search from "./search"
+import Productlist from "./product";
+import About from "./About";
+import Footer from "./Footer";
+
+
+
+
+function App(){
+
+   return(
+    <div>
+    <Navbar></Navbar>
+    <Search></Search>
+    <Productlist></Productlist>
+    <About></About>
+    <Footer></Footer>
+
+
+   </div>
+   )
+
+}
+
+
+export default App
